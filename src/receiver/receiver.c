1bc941b302432a11ad4ffee8d5e792a45c12238f
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-13 21:37:16 +0200

    .

---
+++ src/receiver/receiver.c
@@ -0,0 +1,12 @@
+//
+// Created by Zdeněk Lapeš on 13/10/22.
+// Copyright 2022 <Zdenek Lapes>
+//
+
+#include "stdio.h"
+
+int main(int argc, char *argv[]) {
+    (void )argc;
+    (void )argv;
+    printf("receiver");
+}

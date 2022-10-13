d95bb099e2034d4b070732f9dd271efc80d6ea49
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-14 01:38:44 +0200

    .

--- tests/test_1.py
+++ tests/test_1.py
@@ -1,5 +1,21 @@
+import os
 import pytest
+from pathlib import Path
+from subprocess import PIPE, STDOUT, run, call
 
 
-def test_1():
-    assert 1 == 1
+@pytest.fixture
+def change_test_dir(request):
+    os.chdir(request.config.rootdir)
+    yield
+    os.chdir(request.config.invocation_dir)
+
+
+def test_arguments(change_test_dir):
+    assert call("make") == 0
+    assert call(['./sender', '-u', '127.0.0.1', 'example.com', 'data.txt', './data.txt']) == 0
+    assert call(['echo', 'foo', '|', './sender', 'example.com', 'data.txt']) == 0
+
+    # errors
+    assert call(['./sender', '-u', '127.0.0.1', 'example.com', 'data.txt']) == 0  # TODO: Is this error
+    assert call(['./sender', '-u', '127.0.0.1', 'example.com', 'data.txt', './data.txt', 'ff']) == 1

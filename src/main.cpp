a4e3469ce31a7410c5e1bf05c9852e57f7f83b33
Zdeněk Lapeš <lapes.zdenek@gmail.com>
2022-10-13 16:03:06 +0200

    starting point

---
+++ src/main.cpp
@@ -0,0 +1,13 @@
+//
+// Created by Zdeněk Lapeš on 13/10/22.
+//
+
+#include <iostream>
+
+int main([[maybe_unused]]int argc,
+         [[maybe_unused]]char *argv[]) {
+    using namespace std;
+    cout << "Hello" << endl;
+}
+
+
